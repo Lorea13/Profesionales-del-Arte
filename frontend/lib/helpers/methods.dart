82246e4a2df0e50b15413a594ddc78e15d7d0d32
Lorea13 <lorea.intxausti@opendeusto.es>
2023-04-02 12:34:13 +0200

    Methods y urls terminado

--- frontend/lib/helpers/methods.dart
+++ frontend/lib/helpers/methods.dart
@@ -72,6 +72,75 @@ Future<List<Person>> getPeople(
   return people;
 }
 
+/// Crea una persona
+///
+/// Se nos pasa como parametro el person a crear [person]. Sus campos se crean en un body json. Después llamamos al endpoint gracias a la URI createPersonUri de helpers/urls. Retorna el ID del person creado si la acción se completa correctamente (status code == 200), y 0 en caso contrario (ya que los ids empiezan en 1, no existe el ID 0. Si se retorna este valor, sabremos que se ha producido un error y no realizaremos la acción).
+Future<int> createPerson(Person person) async {
+  Client client = http.Client();
+  var bodyEncoded = jsonEncode({
+    "type": person.type.id.toString(),
+    "name": person.name,
+    "contactDate": person.contactDate.toString(),
+    "contactDescription": person.contactDescription,
+    "projects": person.projects,
+    "webPage": person.webPage,
+    "email": person.email,
+    "phone": person.phone,
+    "notes": person.notes,
+  });
+
+  var response = await client.post(createPersonUri(),
+      headers: {"Content-Type": "application/json"}, body: bodyEncoded);
+
+  if (response.statusCode == 200) {
+    int id = int.parse(json.decode((response).body)['id']);
+    return id;
+  }
+  return 0;
+}
+
+///Modifica una persona
+///
+///Se pasa como parametro el person modificado [person]. Sus campos se crean en un body json. Después llamamos al endpoint gracias a la URI updatePersonUri de helpers/urls, al que le pasamos el id del person a modificar. Retorna true si la operación se completa exitosamente (codigo de respuesta == 200) y false en caso contrario.
+Future<bool> updatePerson(Person person) async {
+  Client client = http.Client();
+  var bodyEncoded = jsonEncode({
+    "type": person.type.id.toString(),
+    "name": person.name,
+    "contactDate": person.contactDate.toString(),
+    "contactDescription": person.contactDescription,
+    "projects": person.projects,
+    "webPage": person.webPage,
+    "email": person.email,
+    "phone": person.phone,
+    "notes": person.notes,
+  });
+
+  var response = await client.put(updatePersonUri(person.id.toString()),
+      headers: {"Content-Type": "application/json"}, body: bodyEncoded);
+
+  if (response.statusCode == 200) {
+    return true;
+  }
+  return false;
+}
+
+///Elimina una persona
+///
+///Se pasa como parametro el id del person a eliminar [id]. Llamamos al endpoint gracias a la URI deletePersonUri de helpers/urls, al que le pasamos el id del person a eliminar. Retorna true si la operación se completa exitosamente (codigo de respuesta == 200) y false en caso contrario.
+Future<bool> deletePerson(int id) async {
+  Client client = http.Client();
+
+  var response = await client.get(deletePersonUri(id.toString()),
+      headers: {"Content-Type": "application/json"});
+
+  if (response.statusCode == 200) {
+    return true;
+  }
+  return false;
+}
+
+
 /// Obtiene todos los castings de la base de datos
 ///
 /// Hace una llamada al endpoint gracias a la URI de helpers/urls allCastings, que nos devuelve una Future List de los castings. Es una Future List debido a que, al ser una llamada a la API, la data no se obtiene de manera inmediata. Por cada elemento de la respuesta, creamos un casting y lo añadimos a [castings], que es lo que retornamos.
@@ -115,7 +184,7 @@ Future<List<Casting>> getCastings(
 Future<int> createCasting(Casting casting) async {
   Client client = http.Client();
   var bodyEncoded = jsonEncode({
-    "date": casting.date,
+    "date": casting.date.toString(),
     "name": casting.name,
     "castingDirector": casting.castingDirector.id.toString(),
     "director": casting.director.id.toString(),
@@ -140,7 +209,7 @@ Future<int> createCasting(Casting casting) async {
 Future<bool> updateCasting(Casting casting) async {
   Client client = http.Client();
   var bodyEncoded = jsonEncode({
-    "date": casting.date,
+    "date": casting.date.toString(),
     "name": casting.name,
     "castingDirector": casting.castingDirector.id.toString(),
     "director": casting.director.id.toString(),
@@ -165,6 +234,218 @@ Future<bool> deleteCasting(int id) async {
   Client client = http.Client();
 
   var response = await client.get(deleteCastingUri(id.toString()),
+      headers: {"Content-Type": "application/json"});
+
+  if (response.statusCode == 200) {
+    return true;
+  }
+  return false;
+}
+
+
+/// Obtiene todos los companys de la base de datos
+///
+/// Hace una llamada al endpoint gracias a la URI de helpers/urls allCompanys, que nos devuelve una Future List de los companys. Es una Future List debido a que, al ser una llamada a la API, la data no se obtiene de manera inmediata. Por cada elemento de la respuesta, creamos un company y lo añadimos a [companys], que es lo que retornamos.
+Future<List<Company>> getCompanys() async {
+  Client client = http.Client();
+
+  List<Company> companys = [];
+
+  List response = json.decode((await client.get(allCompanys())).body);
+
+  for (var company in response) {
+    int id = int.parse(company['id']);
+    String name = company['name'];
+
+    companys.add(Company(
+      id,
+      name,
+    ));
+  }
+  return companys;
+}
+
+
+/// Crea un company
+///
+/// Se nos pasa como parametro el company a crear [company]. Sus campos se crean en un body json. Después llamamos al endpoint gracias a la URI createCompanyUri de helpers/urls. Retorna el ID del company creado si la acción se completa correctamente (status code == 200), y 0 en caso contrario (ya que los ids empiezan en 1, no existe el ID 0. Si se retorna este valor, sabremos que se ha producido un error y no realizaremos la acción).
+Future<int> createCompany(Company company) async {
+  Client client = http.Client();
+  var bodyEncoded = jsonEncode({
+    "name": company.name,
+  });
+
+  var response = await client.post(createCompanyUri(),
+      headers: {"Content-Type": "application/json"}, body: bodyEncoded);
+
+  if (response.statusCode == 200) {
+    int id = int.parse(json.decode((response).body)['id']);
+    return id;
+  }
+  return 0;
+}
+
+///Modifica un company
+///
+///Se pasa como parametro el company modificado [company]. Sus campos se crean en un body json. Después llamamos al endpoint gracias a la URI updateCompanyUri de helpers/urls, al que le pasamos el id del company a modificar. Retorna true si la operación se completa exitosamente (codigo de respuesta == 200) y false en caso contrario.
+Future<bool> updateCompany(Company company) async {
+  Client client = http.Client();
+  var bodyEncoded = jsonEncode({
+    "name": company.name,
+  });
+
+  var response = await client.put(updateCompanyUri(company.id.toString()),
+      headers: {"Content-Type": "application/json"}, body: bodyEncoded);
+
+  if (response.statusCode == 200) {
+    return true;
+  }
+  return false;
+}
+
+///Elimina un company
+///
+///Se pasa como parametro el id del company a eliminar [id]. Llamamos al endpoint gracias a la URI deleteCompanyUri de helpers/urls, al que le pasamos el id del company a eliminar. Retorna true si la operación se completa exitosamente (codigo de respuesta == 200) y false en caso contrario.
+Future<bool> deleteCompany(int id) async {
+  Client client = http.Client();
+
+  var response = await client.get(deleteCompanyUri(id.toString()),
+      headers: {"Content-Type": "application/json"});
+
+  if (response.statusCode == 200) {
+    return true;
+  }
+  return false;
+}
+
+/// Obtiene todos los activityTypes de la base de datos
+///
+/// Hace una llamada al endpoint gracias a la URI de helpers/urls allActivityTypes, que nos devuelve una Future List de los ActivityTypes. Es una Future List debido a que, al ser una llamada a la API, la data no se obtiene de manera inmediata. Por cada elemento de la respuesta, creamos un activityType y lo añadimos a [activityTypes], que es lo que retornamos.
+Future<List<ActivityType>> getActivityTypes() async {
+  Client client = http.Client();
+
+  List<ActivityType> activityTypes = [];
+
+  List response = json.decode((await client.get(allActivityTypes())).body);
+
+  for (var activityType in response) {
+    int id = int.parse(activityType['id']);
+    String name = activityType['name'];
+
+    activityTypes.add(activityType(
+      id,
+      name,
+    ));
+  }
+  return activityTypes;
+}
+
+
+
+/// Obtiene todos los activities de la base de datos
+///
+/// Hace una llamada al endpoint gracias a la URI de helpers/urls allActivities, que nos devuelve una Future List de los activities. Es una Future List debido a que, al ser una llamada a la API, la data no se obtiene de manera inmediata. Por cada elemento de la respuesta, creamos un activity y lo añadimos a [activities], que es lo que retornamos.
+Future<List<Activity>> getActivities(
+  List<ActivityType> activityTypesList, List<Company> companysList) async {
+  Client client = http.Client();
+
+  List<Activity> activities = [];
+
+  List response = json.decode((await client.get(allActivities())).body);
+
+  for (var activity in response) {
+    int id = int.parse(activity['id']);
+    ActivityType type = activityTypesList[activity['type'] - 1];
+    DateTime date = DateTime.parse(activity['date']);
+    String name = activity['name'];
+    Company company = companysList[activity['company'] - 1];
+    int hours = int.parse(activity['hours']);
+    int price = int.parse(activity['price']);
+    int iva = int.parse(activity['iva']);
+    bool invoice = activity['invoice'];
+    bool getPaid = activity['getPaid'];
+    String notes = activity['notes'];
+
+    activities.add(Activity(
+      id,
+      type,
+      date,
+      name,
+      company,
+      hours,
+      price,
+      iva,
+      invoice,
+      getPaid,
+      notes,
+    ));
+  }
+  return activities;
+}
+
+
+/// Crea un activity
+///
+/// Se nos pasa como parametro el activity a crear [activity]. Sus campos se crean en un body json. Después llamamos al endpoint gracias a la URI createActivityri de helpers/urls. Retorna el ID del activity creado si la acción se completa correctamente (status code == 200), y 0 en caso contrario (ya que los ids empiezan en 1, no existe el ID 0. Si se retorna este valor, sabremos que se ha producido un error y no realizaremos la acción).
+Future<int> createActivity(Activity activity) async {
+  Client client = http.Client();
+  var bodyEncoded = jsonEncode({
+    "type": activity.type.id.toString(),
+    "date": activity.date.toString(),
+    "name": activity.name,
+    "company": activity.company.id.toString(),
+    "hours": activity.hours.toString(),
+    "price": activity.price.toString(),
+    "iva": activity.iva.toString(),
+    "invoice": activity.invoice,
+    "getPaid": activity.getPaid,
+    "notes": activity.notes,
+  });
+
+  var response = await client.post(createActivityUri(),
+      headers: {"Content-Type": "application/json"}, body: bodyEncoded);
+
+  if (response.statusCode == 200) {
+    int id = int.parse(json.decode((response).body)['id']);
+    return id;
+  }
+  return 0;
+}
+
+///Modifica un activity
+///
+///Se pasa como parametro el activity modificado [activity]. Sus campos se crean en un body json. Después llamamos al endpoint gracias a la URI updateActivityUri de helpers/urls, al que le pasamos el id del activity a modificar. Retorna true si la operación se completa exitosamente (codigo de respuesta == 200) y false en caso contrario.
+Future<bool> updateActivity(Activity activity) async {
+  Client client = http.Client();
+  var bodyEncoded = jsonEncode({
+    "type": activity.type.id.toString(),
+    "date": activity.date.toString(),
+    "name": activity.name,
+    "company": activity.company.id.toString(),
+    "hours": activity.hours.toString(),
+    "price": activity.price.toString(),
+    "iva": activity.iva.toString(),
+    "invoice": activity.invoice,
+    "getPaid": activity.getPaid,
+    "notes": activity.notes,
+  });
+
+  var response = await client.put(updateActivityUri(activity.id.toString()),
+      headers: {"Content-Type": "application/json"}, body: bodyEncoded);
+
+  if (response.statusCode == 200) {
+    return true;
+  }
+  return false;
+}
+
+///Elimina un activity
+///
+///Se pasa como parametro el id del activity a eliminar [id]. Llamamos al endpoint gracias a la URI deleteActivityUri de helpers/urls, al que le pasamos el id del activity a eliminar. Retorna true si la operación se completa exitosamente (codigo de respuesta == 200) y false en caso contrario.
+Future<bool> deleteActivity(int id) async {
+  Client client = http.Client();
+
+  var response = await client.get(deleteActivityUri(id.toString()),
       headers: {"Content-Type": "application/json"});
 
   if (response.statusCode == 200) {
--- frontend/lib/helpers/urls.dart
+++ frontend/lib/helpers/urls.dart
@@ -7,7 +7,6 @@ Uri allPersonTypes() {
 }
 
 
-
 /// Crea la URI al endpoint de get all people
 ///
 /// Retorna la URI correspondiente [allPeople]
@@ -15,6 +14,31 @@ Uri allPeople() {
   Uri allPeople = Uri.http('127.0.0.1:8000', '/people');
   return (allPeople);
 }
+
+/// Crea la URI al endpoint de update person
+///
+/// Retorna la URI correspondiente [updatePerson]
+Uri updatePersonUri(String id) {
+  Uri updatePerson = Uri.http('127.0.0.1:8000', '/people/update/$id');
+  return (updatePerson);
+}
+
+/// Crea la URI al endpoint de create person
+///
+/// Retorna la URI correspondiente [createPerson]
+Uri createPersonUri() {
+  Uri createPerson = Uri.http('127.0.0.1:8000', '/people');
+  return (createPerson);
+}
+
+/// Crea la URI al endpoint de delete person
+///
+/// Se pasa por parametro el id del person a eliminar [id]. Retorna la URI correspondiente [deletePerson]
+Uri deletePersonUri(String id) {
+  Uri deletePerson = Uri.http('127.0.0.1:8000', '/people/delete/$id');
+  return (deletePerson);
+}
+
 
 
 /// Crea la URI al endpoint de get all castings
@@ -48,4 +72,79 @@ Uri createCastingUri() {
 Uri deleteCastingUri(String id) {
   Uri deleteCasting = Uri.http('127.0.0.1:8000', '/castings/delete/$id');
   return (deleteCasting);
+}
+
+
+/// Crea la URI al endpoint de get all companys
+///
+/// Retorna la URI correspondiente [allCompanys]
+Uri allCompanys() {
+  Uri allCompanys = Uri.http('127.0.0.1:8000', '/companys');
+  return (allCompanys);
+}
+
+
+/// Crea la URI al endpoint de update company
+///
+/// Retorna la URI correspondiente [updateCompany]
+Uri updateCompanyUri(String id) {
+  Uri updateCompany = Uri.http('127.0.0.1:8000', '/companys/update/$id');
+  return (updateCompany);
+}
+
+/// Crea la URI al endpoint de create company
+///
+/// Retorna la URI correspondiente [createCompany]
+Uri createCompanyUri() {
+  Uri createCompany = Uri.http('127.0.0.1:8000', '/companys');
+  return (createCompany);
+}
+
+/// Crea la URI al endpoint de delete company
+///
+/// Se pasa por parametro el id del company a eliminar [id]. Retorna la URI correspondiente [deleteCompany]
+Uri deleteCompanyUri(String id) {
+  Uri deleteCompany = Uri.http('127.0.0.1:8000', '/companys/delete/$id');
+  return (deleteCompany);
+}
+
+/// Crea la URI al endpoint de get all ActivityTypes
+///
+/// Retorna la URI correspondiente [allActivityTypes]
+Uri allActivityTypes() {
+  Uri allActivityTypes = Uri.http('127.0.0.1:8000', '/activityTypes');
+  return (allActivityTypes);
+}
+
+/// Crea la URI al endpoint de get all activities
+///
+/// Retorna la URI correspondiente [allActivities]
+Uri allActivities() {
+  Uri allActivities = Uri.http('127.0.0.1:8000', '/activities');
+  return (allActivities);
+}
+
+
+/// Crea la URI al endpoint de update activity
+///
+/// Retorna la URI correspondiente [updateActivity]
+Uri updateActivityUri(String id) {
+  Uri updateActivity = Uri.http('127.0.0.1:8000', '/acitivites/update/$id');
+  return (updateActivity);
+}
+
+/// Crea la URI al endpoint de create activity
+///
+/// Retorna la URI correspondiente [createActivity]
+Uri createActivityUri() {
+  Uri createActivity = Uri.http('127.0.0.1:8000', '/activities');
+  return (createActivity);
+}
+
+/// Crea la URI al endpoint de delete activity
+///
+/// Se pasa por parametro el id del activity a eliminar [id]. Retorna la URI correspondiente [deleteActivity]
+Uri deleteActivityUri(String id) {
+  Uri deleteActivity = Uri.http('127.0.0.1:8000', '/activites/delete/$id');
+  return (deleteActivity);
 }
